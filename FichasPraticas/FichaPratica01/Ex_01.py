b1454d297bde0e300c35b84a745f115041806f79
Vitor Santos <vmvs007@gmail.com>
2024-09-06 11:59:40 +0100

    FichaPratica01

---
+++ FichasPraticas/FichaPratica01/Ex_01.py
@@ -0,0 +1,13 @@
+# Ler num1
+print("Introduza um número:")
+num1 = int(input())
+
+# ler num2
+print("Introduza outro número:")
+num2 = int(input())
+
+# resultado é a soma de num1 com num2
+resultado = num1 + num2
+
+# apresentar resultado
+print("Soma: ", resultado)
